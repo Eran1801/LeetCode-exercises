bf4ef2010d57f6a1c6e24132af93a8c3474370cc
Eran Levy <57839539+Eran1801@users.noreply.github.com>
2022-03-03 11:54:36 +0200

    Update Two_sum.py

--- Two_sum.py
+++ Two_sum.py
@@ -1,3 +1,12 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+'''
+
 def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         # if we have a list with len of 2, and the both of them is equal the algo wont work so we check
