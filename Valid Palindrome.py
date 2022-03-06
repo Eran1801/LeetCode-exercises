e87aacce952727527f4098384dfd215316ad27dc
Eran Levy <57839539+Eran1801@users.noreply.github.com>
2022-03-06 10:36:11 +0200

    Update Valid Palindrome.py
    improving code

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -7,30 +7,21 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s_low = s.lower()
-
+        
         s_check = str()
 
-        # take down all the non-alphanumeric from the string
-        for char in s_low:
-            if char.isalnum(): # isalnum is a function that return true or false on if the string is Alphanumeric characters that include letters and numbers only.
-                s_check += char
-
-
-        # check if it's a palindrome word by itrate the string in reversed and check equal with the orginal 
-        i = 0 
-        for char in reversed(s_check):
-            if char == s_check[i]:
-                i +=1 
-            else:
-                return False
-
-        return True
+        # take down all the non-alphanumeric from the string AND convert the leeter to lower if it is.
+        for char in s:
+            if char.isalnum():
+                s_check += char.lower()
+        return s_check == s_check[::-1] #  check if it's a palindrome
       
-      
-      '''
-      Python String isalnum()  - https://www.programiz.com/python-programming/methods/string/isalnum
-      Python reversed - https://stackoverflow.com/questions/7961499/best-way-to-loop-over-a-python-string-backwards
-      
-      '''
+        
+        
+   '''
+   Python String isalnum()  - https://www.programiz.com/python-programming/methods/string/isalnum
+   
+   s_check[::-1] - means that same string but in reverse, if s_check was = "Hey" so print(s_check[::-1]) will be "yeH"
+   
+   '''
       
